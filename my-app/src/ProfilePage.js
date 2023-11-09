7bf0f4978f4da26ca131a1b0e417d6ed50b9436a
Kevin Patel <kevin@Kevins-MacBook-Air-2.local>
2023-11-09 16:48:11 -0600

    prof almost

--- my-app/src/ProfilePage.js
+++ my-app/src/ProfilePage.js
@@ -4,33 +4,56 @@ import { FaStar, FaRegStar, FaPhone, FaHeart } from "react-icons/fa";
 
 const ProfilePage = ({ walker }) => {
   // Structure your profile page here using the walker details
+
   return (
-    <div className="profile-container">
-      <header className="profile-header">
-        <img
-          src={walker.imageUrl}
-          alt={walker.name}
-          className="profile-image"
-        />
-        <h1 className="profile-name">{walker.name}</h1>
-        <p className="profile-location">{walker.location.city}</p>
-        <div className="profile-rating">
-          {[...Array(5)].map((star, index) => {
-            return index < walker.rating ? (
-              <FaStar key={index} className="star-filled" />
-            ) : (
-              <FaRegStar key={index} className="star-empty" />
-            );
-          })}
-          <span className="rating-count">({walker.reviews.length})</span>
+    <div class="profile-container">
+      <div class="profile-header">
+        <div className="profile-left">
+          <img
+            src={walker.imageUrl}
+            alt={walker.name}
+            className="profile-image"
+          />
         </div>
-        <button className="contact-button">
-          <FaPhone /> Contact {walker.name.split(" ")[0]}
+        <h1 class="profile-name">{walker.name}</h1>
+        <p class="profile-location">{walker.location.city}</p>
+        <div class="profile-rating">
+          ★★★★★ ({walker.reviews.length})
+          <span class="response-rate">Response Rate: 98%</span>
+        </div>
+        <button
+          class="contact-button"
+          style={{ backgroundColor: "#007bff", color: "white" }}
+        >Book
         </button>
-        <button className="favorite-button">
-          <FaHeart /> {/* Implement favorite logic */}
-        </button>
-      </header>
+        <div class="favorite-icon">♡</div>
+      </div>
+
+      <div class="profile-section">
+        <div class="profile-services">
+          <h2>Services</h2>
+          <p>
+            House Sitting <span class="pricing">$25 per visit</span>
+          </p>
+          <p>
+            Dog Walking <span class="pricing">$15 per visit</span>
+          </p>
+        </div>
+        <div class="profile-availability">
+          <h2>Availability</h2>
+          <p>Days: Mon, Tues, Wed, Sat</p>
+          <p>Time: from 9 AM to 4 PM</p>
+        </div>
+      </div>
+
+      <div class="profile-reviews">
+        <h2>Reviews</h2>
+        <div class="review">
+          <p class="review-author">Grant B.</p>
+          <p class="review-date">Sep 8,2023</p>
+          <p class="review-content">{walker.reviews[0].comment}</p>
+        </div>
+      </div>
     </div>
   );
 };
